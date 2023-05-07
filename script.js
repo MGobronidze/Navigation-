9eeb5716be2b59e49b22d4eb0de9e834501493b6
MGobronidze <90101429+MGobronidze@users.noreply.github.com>
2023-05-07 22:31:37 +0400

    adding toggling on burger

--- script.js
+++ script.js
@@ -3,9 +3,15 @@ let navigationElement = document.querySelector('.navigation')
 let navUlElement=document.querySelector('.nav-ul')
 let navImgElement=document.querySelector('.nav-img')
 
+let firstElement = document.querySelector('.first')
+let secondElement = document.querySelector('.second')
+let thirdElement = document.querySelector('.third')
+
 burgerElement.addEventListener('click',function() {
     navigationElement.classList.toggle('active');
     navUlElement.classList.toggle('display')
     navImgElement.classList.toggle('notDisplay')
-
+    firstElement.classList.toggle('displayFirst')
+    secondElement.classList.toggle('displaySecond')
+    thirdElement.classList.toggle('displayThird')
 })
